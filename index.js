578ba63fb196e7ccc85754bb6ce398207802e5b3
Maria Eugênia <beds03@gmail.com>
2019-10-08 22:32:18 -0300

    created response and route

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("[1] MEU AMBIENTE: ", process.env.NODE_ENV)
+const express = require('express');
+
+const app = express();
+
+app.get('/users', (request, response, next) => {
+    console.log("ENTREI NA PORTA USERS");
+    response.json({ success: true });
+    //console.log(response);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
