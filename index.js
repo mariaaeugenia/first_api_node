79e48da80d4ecc7d7302bc6c386e3510f8cd4764
Maria Eugênia <beds03@gmail.com>
2019-10-10 22:46:43 -0300

    feat: list all users using map

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const express = require('express');
+const app = express();
 //FIREBASE
 const firebase = require('firebase');
 const firebaseConfig = require('./config/firebase');
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 const db = firebaseApp.firestore();
 
-const app = express();
+//SINGLE USER
+// app.get('/users/:id', (request, response, next) => {
+//     const id = request.params.id;
+//     db.collection('users').doc(id).get()
+//       .then(user => {
+//         response.json( user.data() );
+//       })
+//       .catch(err =>  {
+//         response.json( err );
+//       });
+// });
 
+// let data = {
+//   name: 'Kara Denvers',
+//   email: 'kara@email.com'
+// };
+// let setDoc = db.collection('users').doc().set(data);
+
+//LIST OF USERS
 app.get('/users/:id', (request, response, next) => {
-    console.log("ENTREI NA PORTA USERS");
-    const id = request.params.id;
-    db.collection('users').doc(id).get()
-      .then(user => {
-        //console.log(user.data());
-        response.json(user.data());
-      })
+    db.collection('users').get()
+      .then(users => response.json(
+        users.docs.map(user => ({
+          ...user.data(),
+          id: user.id,
+        }))
+      ))
       .catch(err =>  {
-        return response.json( err );
+        response.json( err );
       });
-});
-
-app.get('/users', (request, response, next) => {
-    console.log("ENTREI NA PORTA USERS");
-    response.json({ success: true });
-    //console.log(response);
 });
 
 const port = process.env.PORT || 3000;
