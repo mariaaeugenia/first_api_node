dc8a760908cded1f66b257dee1e2efbd29f94602
Maria Eugênia <beds03@gmail.com>
2019-10-10 21:47:10 -0300

    feat: setup firebase; print user json

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDUaHJbFZ98uGfhHn8xXA1xR9bzn7gv2hY",
+    authDomain: "first-api-node.firebaseapp.com",
+    databaseURL: "https://first-api-node.firebaseio.com",
+    projectId: "first-api-node",
+    storageBucket: "first-api-node.appspot.com",
+    messagingSenderId: "990811020156",
+    appId: "1:990811020156:web:b348afb3504e9831bae2f9"
+};
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+//FIREBASE
+const firebase = require('firebase');
+const firebaseConfig = require('./config/firebase');
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
 
 const app = express();
+
+app.get('/users/:id', (request, response, next) => {
+    console.log("ENTREI NA PORTA USERS");
+    const id = request.params.id;
+    db.collection('users').doc(id).get()
+      .then(user => {
+        //console.log(user.data());
+        response.json(user.data());
+      })
+      .catch(err =>  {
+        return response.json( err );
+      });
+});
 
 app.get('/users', (request, response, next) => {
     console.log("ENTREI NA PORTA USERS");
