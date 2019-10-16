ab3c9f96a9caf089bcea70fd6ba2311ce9180c31
Maria Eugênia <beds03@gmail.com>
2019-10-15 22:41:25 -0300

    create files for create and verify token

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 //FIREBASE
-const firebase = require('firebase');
-const firebaseConfig = require('./config/firebase');
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+const firebase = require('firebase')
+const firebaseConfig = require('./config/firebase')
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
 
-//JWT
-const config = require('./config/default');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
+//BODY PARSER
+const bodyParser = require('body-parser')
+
+const createToken = require('./utils/createToken')
+const verifyToken = require('./middlewares/verifyToken')
 
 
 //SIGN IN WITH EMAIL AND PASSWORD
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.post('/auth', (request, response, next) => {
   db.collection('users')
   .where('email', '==', request.body.email)
@@ -21,73 +22,43 @@ app.post('/auth', (request, response, next) => {
   .get()
     .then(users => {
       if (users.empty) {
-        const message = {message:'Login failed'};
+        const message = {message:'Login failed'}
         return response
           .status(401)
-          .send(message);
+          .send(message)
       }
-      const id = users.docs[0].id;
-      const token = jwt.sign(
-        { id },
-        config.secret,
-        { expiresIn: 300}
-      );
-      const result = {'token':token};
-
-      response.json(result);
+      const [{ id }] = users.docs
+      response.json({token: createToken({ id }) })
     })
     .catch(err =>  {
-      response.json( err );
-    });
-});
-
-
-const verifyJWT = (request, response, next) => {
-  const token = request.headers['x-access-token'];
-  if (!token) {
-    return response
-    .status(401)
-    .send({
-        code: 'not_authorized',
-        message: 'Not authorized'
-      });
-  }
-
-  jwt.verify(token, config.secret, (error, decoded) => {
-    if (error) {
-      return response
-      .status(500)
-      .send({ message: error });
-    }
-    console.log(decoded);
-    next();
-  });
-}
+      response.json( err )
+    })
+})
 
 //SINGLE USER
-app.get('/users/:id', verifyJWT, (request, response, next) => {
-    const id = request.params.id;
+app.get('/users/:id', verifyToken, (request, response, next) => {
+    const id = request.params.id
     db.collection('users').doc(id).get()
       .then(user => {
         if(user.empty) {
                response
                    .sendStatus(404)
-                   .send({ message: 'User not found' });
+                   .send({ message: 'User not found' })
            }
-        response.json( user.data() );
+        response.json( user.data() )
       })
       .catch(err =>  {
         response
           .sendStatus(500)
-          .json( err );
-      });
-});
+          .json( err )
+      })
+})
 
 // let data = {
 //   name: 'Kara Denvers',
 //   email: 'kara@email.com'
-// };
-// let setDoc = db.collection('users').doc().set(data);
+// }
+// let setDoc = db.collection('users').doc().set(data)
 
 //LIST OF USERS
 app.get('/users', (request, response, next) => {
@@ -99,11 +70,11 @@ app.get('/users', (request, response, next) => {
         }))
       ))
       .catch(err =>  {
-        response.json( err );
-      });
-});
+        response.json( err )
+      })
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
+    console.log(`App listening on port ${port}`)
+})
--- utils/createToken.js
+++ utils/createToken.js
@@ -1,7 +1,9 @@
-module.exports = (data, expiresIn = 300) => {
-  return jwt.sign(
-    data,
-    config.secret,
-    { expiresIn}
-  );
-}
+const jwt = require('jsonwebtoken')
+const { secret } = require('../config/default')
+
+module.exports = (data, expiresIn = 300) =>
+  jwt.sign(
+      data,
+      secret,
+      { expiresIn }
+  )
