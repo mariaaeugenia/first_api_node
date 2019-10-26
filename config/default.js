f64acf2dbda58bc2ef113f488a4d26c2a3f1888d
Maria Eugênia <beds03@gmail.com>
2019-10-26 11:38:50 -0300

    added cache

--- config/default.js
+++ config/default.js
@@ -1,3 +1,8 @@
 module.exports = {
-    secret: 'PwCQJdl+BkZo6j1mjb+n3lxqvbkDqD65kva1erB/jzIvzEYtbv87lTzCBBbF6V6gi9Z6OyrjGWhee7ltN9r+1g=='
+    secret: 'PwCQJdl+BkZo6j1mjb+n3lxqvbkDqD65kva1erB/jzIvzEYtbv87lTzCBBbF6V6gi9Z6OyrjGWhee7ltN9r+1g==',
+    cache: {
+        store: 'memory', //CACHE STRATEGY TIME
+        ttl: 600, //CACHE LIFE TIME IN SECONDS
+        max: 100 //ITEMS AMOUNT THAT WILL BE SAVED IN MEMORY
+    }
 }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,23 +1,36 @@
 const User = require('../models/Users')
-const UserModel = new User()
+const userModel = new User()
+const cacheManager = require('cache-manager')
+const { cache } = require('../config/default')
+//CACHE LIBRARY INSTANCE
+const memoryCache = cacheManager.caching(cache)
 
 class UsersControllers {
     static getById(req, res) {
         const id = req.params.id
-        UserModel.get(id)
+        const key = 'user_${id}'
+        memoryCache.get(key, (err, result) => {
+            if (result) {
+                return res.json(result)
+            }
+
+            userModel.get(id)
             .then(user => {
                 if(user.empty) {
                     res
                         .sendStatus(404)
                         .send({ message: 'User not found' })
                 }
-                res.json( user.data() )
+                const userData = user.data()
+                memoryCache.set(key, userData)
+                res.json( userData )
             })
             .catch(err =>  {
                 res
                     .sendStatus(500)
                     .json( err )
             })
+        })
     }
 }
 
